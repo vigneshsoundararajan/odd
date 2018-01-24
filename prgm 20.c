65555d581b6d662cd4bfcb9bb7ca771777732bc6
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-01-24 16:19:42 +0530

    Create prgm 20.c

---
+++ prgm 20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int num,a,i;
+	printf("\n enter a number");
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
+	{
+	printf("\n 5* %d=%d",i,5*i);	
+	}
+	
+	
+	return 0;
+}
