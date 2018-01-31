58ba449b17fd95769a0b977c6cf6d1af4ecdec24
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-01-31 16:28:10 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[30];
+int j,flag=0;
+printf("enter the string\n");
+scanf("%s",&a);
+for(j=0;a[j]!=0;j++)
+{
+if(a[j]=='0'||a[j]=='1'||a[j]=='2'||a[j]=='3'||a[j]=='4'||a[j]=='5'||a[j]=='6'||a[j]=='7'||a[j]=='8'||a[j]=='9')
+{
+flag=flag+1;
+}
+}
+if(flag==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
