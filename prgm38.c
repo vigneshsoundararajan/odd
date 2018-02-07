1fe4008463f93f45c0ff4ab11049463e88d947ab
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-02-07 15:53:32 +0530

    Create prgm38.c

---
+++ prgm38.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int a,b;
+ printf("\n enter the two numbers");
+ scanf("%d%d",&a,&b);
+printf("\n before swapping the values of a and b are %d %d",a,b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("\n after swapping the values of a and b are %d %d",a,b);
+}
