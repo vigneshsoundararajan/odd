91783f36eaeb33cd9f2ccb72b8a9ab36f0303c1c
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-02-09 15:57:07 +0530

    Create prgm39.c

---
+++ prgm39.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int g,i,n;
+	int a[10];
+	printf("\n Enter the numbers");
+	for(i=1;i<10;i++)
+	scanf("%d",&a[i]);
+	a[0]=g;
+	for(i=1;i<10;i++)
+	{
+		if(a[i]>g)
+		{
+		n=a[i];
+		}
+		
+	}
+		printf("\n the lagest among the number is %d",n);
+}
