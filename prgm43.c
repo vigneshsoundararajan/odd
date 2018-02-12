44b066960bf46a0a138af7470ba32ac136404e3e
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-02-12 16:50:42 +0530

    Create prgm43.c

---
+++ prgm43.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	char a[50],b[60];
+	printf("\n enter the two strings ");
+	scanf("%s%s",&a,&b);
+	printf("\n the concartination of two string is %s %s",a,b);
+	
+}
