00227356e228d34c0d87e5d166e00a9a42e90760
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-02-26 11:18:09 +0000

    Create prgm57.c

---
+++ prgm57.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,b=20,t;
+	printf("\n Before swapping the values of a and b is %d %d",a,b);
+	t=a;
+	a=b;
+	b=t;
+	printf("\n after swapping the values of a and b is %d %d",a,b);
+}
