86a981b44f92fef547f8b7c5844dd6fefc4fa489
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-02-28 15:58:00 +0530

    Create prgm 52.c

---
+++ prgm 52.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("\n enter a number between 1 and 10");
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+		printf("\n one");
+		break;
+		case 2:
+		printf("\n two");
+		break;
+		case 3:
+		printf("\n three");
+		break;
+		case 4:
+		printf("\n four");
+		break;
+		case 5:
+		printf("\n five");
+		break;
+		case 6:
+		printf("\n six");
+		break;
+		case 7:
+		printf("\n seven");
+		break;
+		case 8:
+		printf("\n eight");
+		break;
+		case 9:
+		printf("\n nine");
+		break;
+		case 10:
+		printf("\n Ten");
+		break;
+	
+	}
+}
