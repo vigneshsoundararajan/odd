97ea909878318d77d5ac0e8d835bc563f08044fc
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-02-28 16:22:49 +0530

    Create prgm54.c

---
+++ prgm54.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,even;
+printf("\n enter the number");
+scanf("%d",&num);
+if(num%2==0)
+{
+	even=num;
+}
+else
+{
+	even=num-1;
+}
+printf("\n %d",even);
+}
